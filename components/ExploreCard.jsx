78ea939a1a92d99e156d8ae440c0fc44c23cb6fa
Cleo35 <86663165+Cleo35@users.noreply.github.com>
2023-04-13 13:00:29 +0200

    updates

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -25,7 +25,8 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
       </h3>
     ) : (
       <div className="absolute bottom-0 p-8 flex justify-start w-full
-      flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
+      flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]"
+      >
         <div
           className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
         >
